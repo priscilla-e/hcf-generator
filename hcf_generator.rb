ada19b1d33162cd2c05f890a36ffe87cff1148d5
Priscilla Emasoga <priscillaemasoga@gmail.com>
2022-02-21 23:36:18 +0000

    generator implemented

--- hcf_generator.rb
+++ hcf_generator.rb
@@ -3,3 +3,22 @@
 # By: Priscilla Emasoga
 
 
+def gen(max, min)
+  q = nil
+  r = nil
+  possible_hcfs = []
+  while r != 0
+    compute = max / min
+    q = compute.floor()
+    r = max - (min * q)
+    puts "#{max} = #{q} x #{min} + #{r}"
+    possible_hcfs << min
+    max = min
+    min = r
+  end
+  hcfIndex = possible_hcfs.length - 1
+  puts "The hcf is: = #{possible_hcfs[hcfIndex]}"
+end
+
+
+gen(123456789, 456456)
